24fe00b6a8ea2b5b261d3fd3347808e12bec5848
Tim Whiting <tim@whitings.org>
2019-03-29 22:04:05 -0600

    image

--- public/components/MyRecipesComponent.js
+++ public/components/MyRecipesComponent.js
@@ -217,6 +217,7 @@ const MyRecipesComponent = Vue.component("my-recipes-component", {
           </ol>
         </section>
       <input v-if="mode == 'add'" type="file" name="photo" @change="changeImage"></input>
+      <p>Don't add a large image please, it will take a long time and then give you an error"</p>
       <button v-if="mode == 'edit'" @click="updateRecipe">Update Recipe</button>
       <button v-else @click="saveRecipe">Save Recipe</button>
       <br/>
